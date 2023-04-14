422adf21eb107336733153dac95d1c70fcca649a
Ruslan Berenev <rulik8740@yandex.ru>
2023-04-14 23:11:26 +0300

    WHITE_LIST изменен тип на set удалена проверка переменных окружения получение переменных реализовано через os.environ load_dotenv перенесено в основную функцию сообщения об ошибках в логах добавлено на русском функция create_post_from_wall сделана асинхронной в create_post_from_wall исправлена обработка исключения KeyError
    прочее:
        - удалены неактуальные тесты

--- app.py
+++ app.py
@@ -9,25 +9,8 @@ from dotenv import load_dotenv
 from my_logger import logger
 from vk_bot import create_post_from_wall
 
-load_dotenv()
-
-API_TOKEN = os.getenv('API_TOKEN')
-WHITE_LIST = [int(x) for x in os.getenv('WHITE_LIST').split(',')]
-
-
-def check_environment_variables():
-    api_token = os.getenv('API_TOKEN')
-    if not api_token:
-        raise ValueError(
-            'API_TOKEN is not defined in the environment variables'
-        )
-
-    white_list = os.getenv('WHITE_LIST')
-    if not white_list:
-        raise ValueError(
-            'WHITE_LIST is not defined in the environment variables'
-        )
-
+API_TOKEN = os.environ['API_TOKEN']
+WHITE_LIST = {int(x) for x in os.environ['WHITE_LIST'].split(',')}
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -50,20 +33,17 @@ async def handle_photo(message: types.Message):
     file_path = file.file_path
     await file.download()
     logger.debug(f'Загружен файл {file_path}')
-    create_post_from_wall(file_path, text)
-    await asyncio.to_thread(os.remove, file_path)
-    logger.debug(f"File {file_path} was successfully removed")
+    await create_post_from_wall(file_path, text)
 
 
 def main():
-    check_environment_variables()
-    logger.info('Start App')
+    load_dotenv()
+    logger.info('Запуск приложения')
     asyncio.run(
         executor.start_polling(
             dp, loop=asyncio.get_event_loop(), skip_updates=True
         )
     )
-    logger.info('Finish App')
 
 
 if __name__ == '__main__':
